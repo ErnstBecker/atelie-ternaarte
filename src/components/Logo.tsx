27d7774889e7c7881e55f61b96e9a028ac28bbcf
ernstbeckerneto <ernst.becker.neto@gmail.com>
2023-05-02 21:50:31 -0300

    Added new pages and finished navbar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,14 @@
 import LogoImg from '../assets/ternaarte.png'
 
-interface LogoProps {
-  // children?: React.ReactNode;
-  classAdd?: string;
-}
+// interface LogoProps {
+//   // children?: React.ReactNode;
+//   classAdd?: string;
+// }
 
-const Logo = ({classAdd}:LogoProps) : JSX.Element => {
+const Logo = () : JSX.Element => {
   return (
     <>
-      <a href='/' className='relative -top-16 {classAdd}'>
+      <a href='/' className='relative -top-16'>
         <img className="border-[4px] rounded-full border-[#baf5fd] w-40" src={LogoImg} alt="LogoImage" />
       </a>
     </>
